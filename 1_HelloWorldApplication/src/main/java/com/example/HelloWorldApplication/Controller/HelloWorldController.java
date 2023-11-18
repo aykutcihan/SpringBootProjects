0205960e4163453e0189b54f833cbef161dd684b
AykutCihan <aykutcihandemir@protonmail.com>
2023-11-18 19:56:27 +0100

    SpringBootProjects

---
+++ 1_HelloWorldApplication/src/main/java/com/example/HelloWorldApplication/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldApplication.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World";
+    }
+}
